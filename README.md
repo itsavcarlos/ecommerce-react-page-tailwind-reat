caaf711fa1baecfd196a58bbcc8e7ecc8c14b855
Carlos Alberto Lara Silva <kalitos_8321978@hotmail.com>
2022-10-07 18:45:35 -0500

    Dia 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio frontend Mentor
+- [Revisa el desafio aqui](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6)
+- [preview project]()
---
+++ src/components/product/MainProduct.jsx
@@ -0,0 +1,30 @@
+const MainProduct = () => {
+    return (
+        <main>
+            <section>
+                galeria de imagenes
+            </section>
+            <section>
+                <p>Sanker Company</p>
+                <h2>Fall imited Edition Sneakears</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum soluta aut, dolore maxime, atque molestiae excepturi tempora rem cumque veritatis, temporibus nisi vitae similique consequuntur?</p>
+
+                <p>
+                    <span>$ 125.00</span>
+                    <span> 50%</span>
+                </p>
+                <p>$ 250.00</p>
+                <div>
+                    <div>
+                        <button>-</button>
+                        <span>0</span>
+                        <button>+</button>
+                    </div>
+                    <button>Add to cart</button>
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default MainProduct;
